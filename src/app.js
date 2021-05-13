ddebfd5b4f847cbb99522f4c7659938adc0c6207
alpoliakov <alpoliakov73@gmail.com>
2021-05-13 03:02:46 +0300

    feat: Created schemas for user, task, board entities.

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const swaggerUI = require('swagger-ui-express');
 const path = require('path');
 const YAML = require('yamljs');
 const userRouter = require('./resources/users/user.router');
+const startServer = require('./utils/startServer');
 
 const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
@@ -11,13 +12,7 @@ app.use(express.json());
 
 app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
-app.use('/', (req, res, next) => {
-  if (req.originalUrl === '/') {
-    res.send('Service is running!');
-    return;
-  }
-  next();
-});
+app.use('/', startServer);
 
 app.use('/users', userRouter);
 
--- src/resources/users/user.model.js
+++ src/resources/users/user.model.js
@@ -1,12 +1,7 @@
-const uuid = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 class User {
-  constructor({
-    id = uuid(),
-    name = 'USER',
-    login = 'user',
-    password = 'P@55w0rd'
-  } = {}) {
+  constructor({ id = uuidv4(), name = 'USER', login = 'user', password = 'P@55w0rd' } = {}) {
     this.id = id;
     this.name = name;
     this.login = login;
@@ -16,6 +11,10 @@ class User {
   static toResponse(user) {
     const { id, name, login } = user;
     return { id, name, login };
+  }
+
+  static fromRequest(body) {
+    return new User(body);
   }
 }
 
