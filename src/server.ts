e3ad667354c63defe5b0aec8548865516a2083f0
alpoliakov <alpoliakov73@gmail.com>
2021-05-27 13:36:42 +0300

    feat: Added the .env to gitignore.

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
 import app from './app';
 import { PORT } from './common/config';
 
-app.listen(PORT, () => console.log(`🚀 App is running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`🚀 App is running on http://localhost:${PORT || 8080}`));
